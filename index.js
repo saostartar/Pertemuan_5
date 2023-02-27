40a94ac01591cb87e8a124b3cdb183c0da5fe7d8
Christian K <christiankandori@gmail.com>
2023-02-27 16:32:54 +0800

    Pertemuan_ke_5

---
+++ index.js
@@ -0,0 +1,69 @@
+let number = 10;
+const number2 = 10
+console.log(number);
+
+var number1;
+number = 30;
+
+console.log (number);
+console.log (number1)
+console.log (number2)
+
+
+let x;
+console.log(typeof x);
+let number3 = 10;
+console.log(typeof number3);
+let text = "Hello";
+console.log(typeof text);
+let y = true;
+console.log(typeof y);
+let data = null;
+console.log(typeof data);
+const car = 
+{
+    color: "red",
+    price: 10000000,
+};
+
+const text1= "Hello";
+const text2="Hello"
+
+console.log(number2 == number3);
+console.log(number2 == number3);
+console.log(number2 > number3);
+console.log(number2 >= number3);
+console.log(number2 < number3);
+console.log(number2 <= number3);
+console.log(text1 === text2);
+
+
+const arr =[2, 4, 6, 8];
+console.log(arr);
+console.log(arr[0]);
+
+for (let i = arr; i < arr.length; i++)
+{
+    console.log(arr[i])
+}
+
+let language = "German";
+
+if (language === "English")
+{
+    console.log("English");
+}
+else if (language === "Bahasa Indonesia")
+{
+    console.log ("Bahasa Indoensia");
+}
+else
+{
+    console.log ("German");
+}
+
+function greetings(name)
+{
+    console.log(`Hello ${name}`);    
+}
+greetings("Christian Kandori")
